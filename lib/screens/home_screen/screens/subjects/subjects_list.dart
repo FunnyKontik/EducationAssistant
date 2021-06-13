1a5d10a6247d2116e806106ccfab9cfc276ac3df
Hohrenda <dimatkachuk2@gmail.com>
2021-06-13 19:45:59 +0300

    does subject screen

--- lib/screens/home_screen/screens/subjects/subjects_list.dart
+++ lib/screens/home_screen/screens/subjects/subjects_list.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:education_assistant/models/subject_model.dart';
 import 'package:education_assistant/screens/home_screen/screens/subjects/add_subject_screen.dart';
+import 'package:education_assistant/screens/home_screen/screens/subjects/subject_info.dart';
 import 'package:education_assistant/services/subject_service.dart';
 import 'package:education_assistant/utils/navigation_utils.dart';
 import 'package:education_assistant/utils/widget_utils.dart';
@@ -39,7 +40,6 @@ class _SubjectsState extends State<Subjects> {
 }
 
 Widget buildBody(BuildContext context, SubjectService subjectService) {
-
   return StreamBuilder<QuerySnapshot>(
     stream: subjectService.getSubjects(),
     builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
@@ -54,16 +54,19 @@ Widget buildBody(BuildContext context, SubjectService subjectService) {
       return ListView.builder(
         itemCount: snapshot.data.docs.length,
         itemBuilder: (_, index) {
-          final subject = SubjectModel.fromMap(snapshot.data.docs[index].data());
+          final subject =
+              SubjectModel.fromMap(snapshot.data.docs[index].data());
           return Padding(
             padding: const EdgeInsets.symmetric(vertical: 8),
             child: ListTile(
-              title: Text(subject.name),
-              trailing: Icon(Icons.info_outline, color: Colors.blueGrey),
-              onTap: () {
-
-              },
-            ),
+                title: Text(subject.name),
+                trailing: Icon(Icons.info_outline, color: Colors.blueGrey),
+                onTap: () {
+                  NavigationUtils.toScreen(context, screen: SubjectInfo(
+                      subjectModel: SubjectModel.fromMap(
+                          snapshot.data.docs[index].data()))
+                  );
+                  }),
           );
         },
       );
